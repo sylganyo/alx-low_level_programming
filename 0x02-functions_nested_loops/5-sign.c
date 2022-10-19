fd982bd01856a4e1cdd43bac86cc1f70743c10fa
sylganyo <sylvanusg22@yahoo.com>
2022-10-19 00:32:59 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,27 +2,27 @@
 
 /**
  * print_sign - prints the sign of a number
- * @n: the num to checked for
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * @n: the number of the sign to be printed
+ * Return: 1 if number is greater than zero
+ * 0 of number is zero
+ * -1 if number is less than zero
  */
 
 int print_sign(int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else if (n > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
 	else
 	{
-		_putchar('_');
+		_putchar('-');
 		return (-1);
 	}
 }
